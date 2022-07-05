eef5751121af7e86383d9c354efc76040beb7ce1
Vojtech Oupicky <voj.oup@gmail.com>
2022-07-05 22:19:27 +0200

    Remove console.log

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  console.log("Middleware", req);
   if (req.nextUrl.pathname.startsWith("/r/")) {
     const slug = req.nextUrl.pathname.split("/").pop();
 
