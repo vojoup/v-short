a57cc5c99739e25767b36df95fe74fe13be5e36c
Vojtech Oupicky <voj.oup@gmail.com>
2022-07-05 22:20:04 +0200

    Prevent generating duplicates

---
+++ src/components/Form.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { ChangeEventHandler } from "react";
+import { trpc } from "../utils/trpc";
+
+const Form = () => {
+  const [url, setUrl] = React.useState("");
+  const [slug, setSlug] = React.useState("");
+  const utils = trpc.useContext();
+
+  const createSlugMutation = trpc.useMutation(["shortLink.add"]);
+  const { data: existingSlug } = trpc.useQuery(
+    ["shortLink.getBySlug", { slug }],
+    {
+      retry: false,
+    }
+  );
+
+  const onUrlChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const onSlugChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value } = e.target;
+    setSlug(value);
+    utils.invalidateQueries("shortLink.getBySlug");
+  };
+
+  const onGenerate = () => {
+    createSlugMutation.mutate({ url, slug });
+  };
+
+  return (
+    <main className="flex gap-2 flex-col">
+      {createSlugMutation.isError ? (
+        <span className="text-red-500">Something went wrong!</span>
+      ) : null}
+      <input
+        className="border-blue-500 border-solid border-2 rounded-md py-2 px-3 leading-tight w-full"
+        id="url"
+        type="url"
+        value={url}
+        name="url-input"
+        onChange={onUrlChange}
+        placeholder="URL"
+        autoFocus
+      />
+      <input
+        className={`${
+          !!existingSlug ? "border-red-700" : "border-blue-500"
+        } border-solid border-2 rounded-md py-2 px-3 leading-tight w-full`}
+        id="slug"
+        type="slug"
+        value={slug}
+        name="slug-input"
+        onChange={onSlugChange}
+        placeholder="Slug"
+      />
+      {!!existingSlug && (
+        <label htmlFor="slug-input" className="text-red-700">
+          This slug is taken!
+        </label>
+      )}
+      <button
+        id="generate"
+        className="text-white bg-blue-500 rounded-md py-2 px-3 hover:bg-blue-600 disabled:cursor-not-allowed disabled:bg-red-700"
+        onClick={onGenerate}
+        disabled={!!existingSlug}
+      >
+        {createSlugMutation.isLoading ? "Generating..." : "Generate"}
+      </button>
+      {createSlugMutation.isSuccess ? (
+        <div>Shortened: {createSlugMutation.data?.slug}</div>
+      ) : null}
+    </main>
+  );
+};
+
+export { Form };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
-import { ChangeEventHandler } from "react";
-import { trpc } from "../utils/trpc";
+
+import { Form } from "../components/Form";
 
 const Home: NextPage = () => {
-  const crerateSlugMutation = trpc.useMutation(["shortLink.add"]);
-
-  const [url, setUrl] = React.useState("");
-  const [slug, setSlug] = React.useState("");
-
-  const onUrlChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setUrl(e.target.value);
-  };
-  const onSlugChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setSlug(e.target.value);
-  };
-
-  const onGenerate = () => {
-    crerateSlugMutation.mutate({ url, slug });
-  };
-
   return (
     <>
       <Head>
@@ -28,52 +12,12 @@ const Home: NextPage = () => {
         <meta name="description" content="An experiment with the t3 stack" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <div className="container flex flex-col items-center justify-center min-h-screen p-10 px-0 mx-auto md:py-20 md:p-10 md:px-0">
         <h1 className="font-extrabold text-center text-7xl">
           <span className="text-blue-500">v-short</span>
         </h1>
-
         <h3 className="items-center m-5 text-3xl">Create a new short link</h3>
-
-        <main className="flex gap-2 flex-col">
-          {crerateSlugMutation.isError ? (
-            <span className="text-red-500">Something went wrong!</span>
-          ) : null}
-
-          <input
-            className="border-blue-500 border-solid border-2 rounded-md py-2 px-3 leading-tight w-full"
-            id="url"
-            type="url"
-            value={url}
-            name="url-input"
-            onChange={onUrlChange}
-            placeholder="URL"
-            autoFocus
-          />
-          <input
-            className="border-blue-500 border-solid border-2 rounded-md py-2 px-3 leading-tight w-full"
-            id="slug"
-            type="slug"
-            value={slug}
-            name="slug-input"
-            onChange={onSlugChange}
-            placeholder="Slug"
-            autoFocus
-          />
-
-          <button
-            id="generate"
-            className="text-white bg-blue-500 rounded-md py-2 px-3 hover:bg-blue-500 hover:bg-blue-600"
-            onClick={onGenerate}
-          >
-            {crerateSlugMutation.isLoading ? "Generating..." : "Generate"}
-          </button>
-
-          {crerateSlugMutation.isSuccess ? (
-            <div>Shortened: {crerateSlugMutation.data?.slug}</div>
-          ) : null}
-        </main>
+        <Form />
       </div>
     </>
   );
